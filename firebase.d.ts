54d606adff7ec093386f682a33505dda57115986
Rogers <mohammedrogers579@gmail.com>
2025-12-12 13:25:50 -0800

    setup firebase database and stored user data, fetchCurrent users etc

---
+++ firebase.d.ts
@@ -0,0 +1,5 @@
+declare module 'firebase/app';
+declare module 'firebase/auth';
+declare module 'firebase/firestore';
+declare module 'firebase/database';
+declare module 'firebase/storage';
--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,12 +1,14 @@
-// src/firebase-config.js
+// @ts-nocheck
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEek_kwT2Fgeg-zhyrqm2z1Q6XmVHv2Tk",
   authDomain: "vyon-streaming-app-project-id.firebaseapp.com",
   projectId: "vyon-streaming-app-project-id",
+  databaseURL: "https://vyon-streaming-app-project-id-default-rtdb.europe-west1.firebasedatabase.app/",
   storageBucket: "vyon-streaming-app-project-id.firebasestorage.app", // fix later
   messagingSenderId: "410488876628",
   appId: "1:410488876628:web:e8cbccfa9c0ef9a43a07c1",
@@ -16,4 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getDatabase(app);
+
